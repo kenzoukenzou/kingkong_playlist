b3c0496b81606ff2963ead36ae9190c534fbea31
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-09 20:56:23 +0900

    fix: add route path on Navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -4,7 +4,11 @@
       dark
     >
     <v-spacer></v-spacer>
-    <v-toolbar-title class="font-weight-bold">キングコングプレイリスト</v-toolbar-title>
+    
+    <router-link :to="{ name: 'VideoList' }" class="white--text text-decoration-none">
+      <v-toolbar-title class="font-weight-bold">キングコングプレイリスト </v-toolbar-title>
+    </router-link>
+    
     <v-spacer></v-spacer>
   </v-app-bar>
 </template>
