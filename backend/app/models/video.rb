d49b7f0c250e71f598ca565ab2f962fd3a63001d
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-10 10:59:53 +0900

    annotate --models

--- backend/app/models/video.rb
+++ backend/app/models/video.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: videos
+#
+#  id          :integer          not null, primary key
+#  title       :string           not null
+#  youtube_key :string           not null
+#  thumbnail   :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  embed_url   :string
+#
+
 class Video < ApplicationRecord
   validates :title, :youtube_key, presence: true
 end
