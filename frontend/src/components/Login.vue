6c77ddfc4ddccba6b9b8538cf7e6c3e8a1b81c1c
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-10 21:15:11 +0900

    fix firebase config source add login method

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -2,21 +2,41 @@
   <v-form class="mt-3">
     <v-text-field
       label="Email"
+      v-model="email"
       required
     >
     </v-text-field>
     <v-text-field
       label="Password"
+      v-model="password"
       required
       type="password"
     >
     </v-text-field>
-    <v-btn dark>ログイン</v-btn>
+    <v-btn dark @click="login">ログイン</v-btn>
   </v-form>
 </template>
 
 <script>
+import firebase from '@/plugins/firebase';
+
 export default {
   name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          this.$router.push("/");
+        })
+    }
+  }
 }
 </script>
--- frontend/src/plugins/firebase.js
+++ frontend/src/plugins/firebase.js
@@ -1,14 +1,17 @@
 import firebase from "firebase/app"
 import "firebase/auth"
 
-const fbConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGE_SENDER_ID,
+const firebaseConfig = {
+  apiKey: "AIzaSyAMZnhhd4dAFgqgrDv-AEwZhI0QYA6lun0",
+  authDomain: "playlist-2bf49.firebaseapp.com",
+  databaseURL: "https://playlist-2bf49.firebaseio.com",
+  projectId: "playlist-2bf49",
+  storageBucket: "playlist-2bf49.appspot.com",
+  messagingSenderId: "514907771294",
+  appId: "1:514907771294:web:048c0bd181cbfd2692468f",
+  measurementId: "G-CSYTX3LV2R"
 };
-firebase.initializeApp(fbConfig)
+
+firebase.initializeApp(firebaseConfig)
 
 export default firebase
