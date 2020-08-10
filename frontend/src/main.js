c221e33e3e146348d7f52297f1bacd8002d996bc
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-10 14:57:46 +0900

    vue use vue-youtube

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,16 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import VueYoutube from 'vue-youtube'
 
 Vue.config.productionTip = false
+Vue.use(VueYoutube)
 
+// Filters
 Vue.filter('truncate', function(value, length){
   if (value.length <= length) {
     return value;
   } else {
     return value.substring(0, 15) + '...';
   }
-
 })
 
 new Vue({
