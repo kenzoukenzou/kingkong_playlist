0da104e28e0c68b383e8e9da52eebd6757b6a72d
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-10 12:07:27 +0900

    add backend methods

--- backend/app/models/video.rb
+++ backend/app/models/video.rb
@@ -13,4 +13,35 @@
 
 class Video < ApplicationRecord
   validates :title, :youtube_key, presence: true
+  validates :youtube_key, uniqueness: true
+  
+  YOUTUBE_ENDPOINT = 'https://www.googleapis.com/youtube/v3/videos'
+  YOUTUBE_PARAMS = { part: 'snippet', type: 'video', key: ENV['YOUTUBE_API_KEY'] }
+
+  def save_with_youtube_data(params)
+    ActiveRecord::Base.transaction do
+      assign_attributes(params)
+      result_hash = fetch_youtube_api
+      set_video_data(result_hash)
+      save
+    end
+  end
+
+  private
+    def fetch_youtube_api
+      uri = URI(YOUTUBE_ENDPOINT)
+      params = YOUTUBE_PARAMS.merge(id: youtube_key)
+
+      uri.query = URI.encode_www_form(params)
+      res = Net::HTTP.get_response(uri)
+      JSON.parse(res.body)
+    end
+
+    def set_video_data(result_hash)
+      return if result_hash['items'].blank?
+
+      self.title = result_hash['items'][0]['snippet']['title']
+      self.thumbnail = result_hash['items'][0]['snippet']['thumbnails']['medium']['url']
+      self.embed_url = "https://www.youtube.com/embed/#{self.youtube_key}"
+    end
 end
