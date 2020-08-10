ac28f95ae9a0eb0a21a973554a5e80ae234d9787
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-10 19:23:36 +0900

    add login component

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-form class="mt-3">
+    <v-text-field
+      label="Email"
+      required
+    >
+    </v-text-field>
+    <v-text-field
+      label="Password"
+      required
+      type="password"
+    >
+    </v-text-field>
+    <v-btn dark>ログイン</v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+}
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ import VideoList from '@/components/VideoList'
 import VideoShow from '@/components/VideoShow'
 import VideoNew from '@/components/VideoNew'
 import About from '@/components/About'
+import Login from '@/components/Login'
 
 Vue.use(VueRouter)
 
@@ -27,6 +28,11 @@ Vue.use(VueRouter)
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
