9ae4cc5d917858218555ab4ac362204ffa103fa1
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-12 19:09:34 +0900

    feat: bookmark追加時にサムネイルをセットする処理を追加 memo: 関連付けを整理した方が良さそう

--- backend/app/models/playlist.rb
+++ backend/app/models/playlist.rb
@@ -13,4 +13,9 @@
 class Playlist < ApplicationRecord
   validates :title, presence: true
   has_many :bookmarks
+
+
+  def set_first_bookmark_thumbnail
+    update(thumbnail: bookmarks.first.video.thumbnail)
+  end
 end
