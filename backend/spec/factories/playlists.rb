e725f70e409de6d3cfe59048ff5155a1b93a233a
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-14 09:45:19 +0900

    test: ファクトリを一式用意

---
+++ backend/spec/factories/playlists.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: playlists
+#
+#  id          :integer          not null, primary key
+#  title       :string           not null
+#  description :text
+#  thumbnail   :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+FactoryBot.define do
+  factory :playlists do
+    title { '梶原工業どんがらがっしゃん珍道中' }
+    description { '梶原工業がロレックスを買う話' }
+    thumbnail { 'https://i.ytimg.com/vi/HcFrYRFu2-w/mqdefault.jpg' }
+  end
+end
