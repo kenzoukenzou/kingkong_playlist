58ff1cdb49410c4dd20b49695ab4e320f994289f
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-14 14:18:28 +0900

    test: レスポンスボディをチェックするテストを追加

--- backend/spec/requests/playlists_spec.rb
+++ backend/spec/requests/playlists_spec.rb
@@ -5,13 +5,21 @@ describe "PlaylistAPI" do
     before {
       create_list(:playlist, 10)
     }
-    subject { get v1_playlists_path } 
-    it { is_expected.to eq 200 }
+    it do
+      get v1_playlists_path
+      expect(response.status).to eq 200
+    end
   end
 
   describe "show" do
     let!(:playlist) { create(:playlist, title: 'V1API_SHOW_TEST') }
-    subject { get v1_playlist_path(playlist) }
-    it { is_expected.to eq 200 }
+    it do
+      get v1_playlist_path(playlist)
+      expect(response.status).to eq 200
+    end
+    it 'レスポンスがプレイリストタイトルを含んでいること' do
+      get v1_playlist_path(playlist)
+      expect(response.body).to include 'V1API_SHOW_TEST'
+    end
   end
 end
--- backend/spec/requests/videos_spec.rb
+++ backend/spec/requests/videos_spec.rb
@@ -5,13 +5,21 @@ describe "VideoAPI" do
     before {
       create_list(:video, 10)
     }
-    subject { get v1_videos_path } 
-    it { is_expected.to eq 200 }
+    it do
+      get v1_videos_path
+      expect(response.status).to eq 200
+    end
   end
 
   describe "show" do
     let!(:video) { create(:video, title: 'V1API_SHOW_TEST') }
-    subject { get v1_video_path(video) }
-    it { is_expected.to eq 200 }
+    it do
+      get v1_video_path(video)
+      expect(response.status).to eq 200
+    end
+    it 'レスポンスが動画タイトルを含んでいること' do
+      get v1_video_path(video)
+      expect(response.body).to include 'V1API_SHOW_TEST'
+    end
   end
 end
