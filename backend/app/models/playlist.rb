51184a03d99bb8c495cd32258e9bfead3399a7d5
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-21 18:43:07 +0900

    feat: 他のプレイリストを返すメソッドを定義

--- backend/app/models/playlist.rb
+++ backend/app/models/playlist.rb
@@ -11,9 +11,14 @@
 #
 
 class Playlist < ApplicationRecord
+  DISPLAY_NUM = 10
   validates :title, presence: true
   has_many :bookmarks
   has_many :videos, through: :bookmarks
+
+  def others
+    Video.where.not(id: id).first(DISPLAY_NUM)
+  end
 
   def set_first_video_thumbnail
     update(thumbnail: videos.first.thumbnail)
