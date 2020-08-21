dd69e7eaf75d5701ebb926a577371f45c687099e
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-21 21:32:01 +0900

    fix: プレイリスト一覧ページのデザイン修正

--- frontend/src/components/Playlists.vue
+++ frontend/src/components/Playlists.vue
@@ -4,45 +4,18 @@
       <v-tab to="/">動画一覧</v-tab>
       <v-tab to="/playlists">プレイリスト一覧</v-tab>
     </v-tabs>
-    <v-row>
-      <v-col
-        class="col-lg-3 col-md-4n"
-        v-for="p in playlists"
-        :key="p.id"
-      >
-        <router-link :to="{ name: 'PlaylistShow', params: { id: p.id }}">
-          <v-card
-            class="mx-auto"
-          >
-            <v-img
-              class="white--text align-end"
-              height="200px"
-              :src="p.thumbnail"
-            >
-            </v-img>
-            <v-card-subtitle class="pb-0">{{ p.created_at | formatDateTime }}</v-card-subtitle>
-            <v-card-text class="text--primary pb-0">
-              <div>{{ p.title | truncate(15) }}</div>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn
-                color="pink"
-                text
-              >
-                Explore
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </router-link>
-      </v-col>
-    </v-row>
+    <PlaylistCards :playlists="playlists" />
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import PlaylistCards from './PlaylistCards'
 
 export default {
+  components: {
+    PlaylistCards,
+  },
   data() {
     return {
       playlists: []
