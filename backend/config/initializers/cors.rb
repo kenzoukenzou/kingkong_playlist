4a6823cc6423cdc7af85ff1721b61eb1778fd363
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-21 21:17:37 +0900

    fix: cors.rb を修正

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    if Rails.env.production?
+      origins 'https://playlist-2bf49.web.app'
+    else
+      origins 'http://localhost:8080'
+    end
 
     resource '*',
       headers: :any,
