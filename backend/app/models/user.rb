d1b036f4c1f8d7f4f8f5aad6c48ee0ff5f00c397
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-27 15:53:36 +0900

    feat: バリデーションを追加

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -9,4 +9,6 @@
 #
 
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
