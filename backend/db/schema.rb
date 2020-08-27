636367145cb99fd3695b3f3056891890cf46b2b3
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-27 15:51:01 +0900

    feat: userモデルを作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_130330) do
+ActiveRecord::Schema.define(version: 2020_08_27_064900) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.text "content", null: false
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2020_08_11_130330) do
     t.string "title", null: false
     t.text "description"
     t.string "thumbnail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ backend/spec/factories/users.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+FactoryBot.define do
+  factory :user do
+    email { "MyString" }
+  end
+end
