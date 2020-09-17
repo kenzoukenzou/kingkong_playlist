ec755b7fa8135cd98518f4a4203b64a7f93e63a6
kenzo <kenzou.kenzou104809@gmail.com>
2020-09-17 21:06:03 +0900

    feat: wheneverを使って定期的に処理を実行させる

---
+++ backend/config/schedule.rb
@@ -0,0 +1,27 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+
+# Rails.rootを使用するために必要
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+rails_env = ENV['RAILS_ENV'] || :development
+set :environment, rails_env
+set :output, "#{Rails.root}/log/cron.log"
+
+every :monday, at: '12pm' do
+  rake "video:upload_video"
+end
+
+# Learn more: http://github.com/javan/whenever
