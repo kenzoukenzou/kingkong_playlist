a4c12153bc042f7f21ae26c835d6eabe6bc6346a
kenzo <kenzou.kenzou104809@gmail.com>
2020-09-20 09:42:34 +0900

    fix: Aboutページのリンクをリポジトリに変更

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -2,7 +2,7 @@
   <div class="mt-7">
     <p>当サイトは、運営人が好きな「毎週キングコング」の動画だけが並ぶだけのサイトです。勉強と趣味目的で、Vue.jsとRuby on Railsを使って作りました。</p>
     <p>Twitter: <a href="https://twitter.com/kenzoooooB">https://twitter.com/kenzoooooB</a></p>
-    <p>GitHub: <a href="https://github.com/kenzoukenzou">https://github.com/kenzoukenzou</a></p>
+    <p>GitHub: <a href="https://github.com/kenzoukenzou/kingkong_playlist">https://github.com/kenzoukenzou/kingkong_playlist</a></p>
   </div>
 </template>
 
